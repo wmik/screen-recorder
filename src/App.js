634d4b9a6b1fe7d8a12bd9652b48f587832219ff
wmik <williemik.wmik@gmail.com>
2020-05-12 23:03:40 +0300

    feat: recorder
    - Enable video/screen recording

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function App() {
   let [isRecording, setIsRecording] = React.useState(false);
+  let videoRef = React.useRef();
+  let streamRef = React.useRef();
+  let recorderRef = React.useRef();
 
   function handleStart() {
     setIsRecording(true);
@@ -10,6 +13,42 @@ export default function App() {
   function handleStop() {
     setIsRecording(false);
   }
+
+  React.useEffect(() => {
+    async function startRecording() {
+      streamRef.current = await navigator.mediaDevices.getDisplayMedia({
+        video: { mediaSource: 'screen' }
+      });
+      recorderRef.current = new MediaRecorder(streamRef.current);
+      let chunks = [];
+
+      function handleDataAvailable(e) {
+        chunks.push(e.data);
+      }
+
+      function handleStopRecorder() {
+        let [sampleChunk] = chunks;
+        let recordingBlob = new Blob(chunks, { type: sampleChunk.type });
+        videoRef.current.src = URL.createObjectURL(recordingBlob);
+      }
+
+      recorderRef.current.addEventListener(
+        'dataavailable',
+        handleDataAvailable
+      );
+      recorderRef.current.addEventListener('stop', handleStopRecorder);
+      recorderRef.current.start();
+    }
+
+    if (isRecording) {
+      startRecording();
+    }
+
+    if (!isRecording && recorderRef.current) {
+      recorderRef.current.stop();
+      streamRef.current.getVideoTracks()[0].stop();
+    }
+  }, [isRecording]);
 
   return (
     <article>
@@ -21,6 +60,7 @@ export default function App() {
           Stop recording
         </button>
       </section>
+      <video ref={videoRef} />
     </article>
   );
 }
